f33e85611dafca70f32165b5e493f5e213b9e68a
Rune Bekkevold <rune.bekkevold@nordicsemi.no>
2024-05-29 23:06:11 +0200

    renamed repo

--- test/loadPlotPcmData.py
+++ test/loadPlotPcmData.py
@@ -7,14 +7,35 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from pcmAnalyzer import pcmAnalyzer
 
+
+def get25LastFiles(folder_path):
+    # Get all the filenames in the folder
+    filenames = os.listdir(folder_path)
+
+    # order the filenames in ascending order by the number in the filename
+    sorted_filenames = sorted(filenames, key=lambda x: int(x.split('_')[1].split('.')[0]), reverse=False)
+
+    # Get the 25 highest numbered filenames
+    return sorted_filenames
+
+
 adcResolution = 12
 adcSampleRate = 1e6
 
 
 pp = PdfPages('analysisResults.pdf')
 # read and plot al the data files
-for i in range(1, 25):
-    pcmVector = pd.read_csv('data/data_' + str(i) + '.csv', delimiter=',', header=None)
+for i in range(0, 26):
+    # Laod the PCM data from ATE
+    #pcmVector = pd.read_csv('/home/rune/work/githubRune75/pcmAnalyzer/data/ATE/data_' + str(i) + '.csv', delimiter=',', header=None)
+    
+    # # Load the PCM data from NI
+    # folder_path = '/home/rune/work/githubRune75/pcmAnalyzer/data/NI'
+    # Load the PCM data from NI
+    folder_path = '/home/rune/work/githubRune75/pcmAnalyzer/data/ATE'
+    files = get25LastFiles(folder_path)
+    pcmVector = pd.read_csv(folder_path + '/' + files[i-1], delimiter=',', header=None)
+    
     pcmVector = pcmVector.values.flatten()[:8192]
     
     # remove the DC offset from the signal
